e642e6293bb44fab6d5922a67563a5401f827d5f
ReportsAdmin <89199970+ReportsAdmin@users.noreply.github.com>
2021-08-24 10:44:16 +0530

    Update fKPIs.sql

--- models/Lazurde_UAE/Tables/fKPIs.sql
+++ models/Lazurde_UAE/Tables/fKPIs.sql
@@ -7,17 +7,17 @@ Select Halo_country, KPI, XTD, date, Value,	 prevvalue, prevyearvalue, Startdate
 PrevX_Startdate, PrevX_Enddate, PrevYearX_Startdate, PrevYearX_Enddate, Value_DateRange,
 PrevValue_DateRange, PrevYearValue_DateRange, Change_PrevX, Change_PrevYearX	, KPI_ID, KPI_FORMAT
 from `noted-computing-279322:halo_1_1_lazurdeuaetest.fKPIsCalc`
-
+union all
 Select Halo_country, KPI, XTD, date, Value,	 prevvalue, prevyearvalue, Startdate,	 Enddate,
 PrevX_Startdate, PrevX_Enddate, PrevYearX_Startdate, PrevYearX_Enddate, Value_DateRange,
 PrevValue_DateRange, PrevYearValue_DateRange, Change_PrevX, Change_PrevYearX	, KPI_ID, KPI_FORMAT
 from `noted-computing-279322:halo_1_1_lazurdeuaetest.fKPIsCurrent`
-
+union all
 Select Halo_country, KPI, XTD, date, Value,	 prevvalue, prevyearvalue, Startdate,	 Enddate,
 PrevX_Startdate, PrevX_Enddate, PrevYearX_Startdate, PrevYearX_Enddate, Value_DateRange,
 PrevValue_DateRange, PrevYearValue_DateRange, Change_PrevX, Change_PrevYearX	, KPI_ID, KPI_FORMAT
 from `noted-computing-279322:halo_1_1_lazurdeuaetest.fKPIsFacebook`
-
+union all
 Select Halo_country, KPI, XTD, date, Value,	 prevvalue, prevyearvalue, Startdate,	 Enddate,
 PrevX_Startdate, PrevX_Enddate, PrevYearX_Startdate, PrevYearX_Enddate, Value_DateRange,
 PrevValue_DateRange, PrevYearValue_DateRange, Change_PrevX, Change_PrevYearX	, KPI_ID, KPI_FORMAT
